3f7b2d0463c160f495778b808cd4a92dc4b8d206
Mykhailo Kalchenko <mihailkalchenko01@gmail.com>
2022-12-13 18:41:07 +0200

    added env

--- apparatus_bot/main.py
+++ apparatus_bot/main.py
@@ -1,19 +1,35 @@
 import logging
 from telegram import Update
 from telegram.ext import *
+import os
+from dotenv import load_dotenv
+
+project_folder = os.path.expanduser('~/my-project-dir')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.env'))
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Привіт. Ви хочете грати у гру? Напишіть так, якщо так. Лівніть, якщо ні.")
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token('').build()
+    
+    BOT_TOKEN = os.getenv('BOT_TOKEN')
+    print(BOT_TOKEN)
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+    
     
     start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
+    echo_handler = MessageHandler(filters.TEXT & (~filters.COMMAND), echo)
     
+    application.add_handler(start_handler)
+    application.add_handler(echo_handler)
+
     application.run_polling()
